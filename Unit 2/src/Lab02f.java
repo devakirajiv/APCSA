e85ed0d71f227e4cf98173fa42bf9b49aaac35f1
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-06 09:24:40 -0800

    Finished Lab 02f

---
+++ Unit 2/src/Lab02f.java
@@ -0,0 +1,34 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab02f
+{
+	public static void main( String[] args )
+   {
+		Line test = new Line(0,0,0,0);
+		
+		test.setCoordinates(4, 3, 2, 5);
+		test.calculateSlope();
+		test.print();
+		
+		test.setCoordinates(10, 6, 4, 1);
+		test.calculateSlope();
+		test.print();
+		
+		test.setCoordinates(3, 5, 1, 4);
+		test.calculateSlope();
+		test.print();
+		
+		
+		
+		
+		//add test cases
+		
+		
+		
+		
+	}
+}
