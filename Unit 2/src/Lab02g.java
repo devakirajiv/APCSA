2ba461017548f0c4776d1bea1bac90e9f1ac93ac
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-07 10:07:22 -0800

    finished lab02g

---
+++ Unit 2/src/Lab02g.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab02g
+{
+	public static void main( String[] args )
+	{
+		Fahrenheit test = new Fahrenheit();
+		
+		test.setFahrenheit(60);
+		test.getCelsius();
+		test.print();
+		
+		test.setFahrenheit(75);
+		test.getCelsius();
+		test.print();
+		
+		test.setFahrenheit(50);
+		test.getCelsius();
+		test.print();
+		//add test cases		
+	}
+}
