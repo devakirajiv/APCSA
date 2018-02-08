083efe5558661f5b6e8c02420984a753688e9fb9
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-08 08:37:15 -0800

    Finished Lab 03d

--- Unit 3/src/Lab03d.java
+++ Unit 3/src/Lab03d.java
@@ -12,6 +12,30 @@ public class Lab03d
 {
 	public static void main( String[] args )
 	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter X1 :: ");
+		int x1 = keyboard.nextInt();
+		
+		System.out.println("Enter Y1 :: ");
+		int y1 = keyboard.nextInt();
+		
+		System.out.println("Enter X2 :: ");
+		int x2 = keyboard.nextInt();
+		
+		System.out.println("Enter Y2 :: ");
+		int y2 = keyboard.nextInt();
+		
+		Distance test = new Distance(0,0,0,0);
+		
+		test.setCoordinates(x1, y1, x2, y2);
+		test.calcDistance();
+		test.print();
+		
+		
+		
+		
+		
 		//add test cases
 		
 		
