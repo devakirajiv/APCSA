81df14eb364748440cdb5c600e60d94482d195be
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-14 10:16:08 -0800

    Finished Lab 08i

---
+++ Unit 6/src/Lab08i.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.lang.Math;
+
+public class Lab08i
+{
+	public static void main ( String[] args )
+	{
+		
+		Prime test = new Prime(0);
+		
+		test.setPrime(24);
+		out.println(test);
+		
+		test.setPrime(7);
+		out.println(test);
+		
+		test.setPrime(100);
+		out.println(test);
+		
+		test.setPrime(113);
+		out.println(test);
+		
+		test.setPrime(65535);
+		out.println(test);
+		
+		//add test cases
+		
+		
+		
+		
+		
+		
+		
+	}	
+}
