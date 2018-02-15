c3b5b8f6beac6648ec84dddb7fb2558d63325ffa
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-15 09:03:13 -0800

    Finished Lab 09f

---
+++ Unit 6/src/LetterRemover.java
@@ -0,0 +1,51 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class LetterRemover
+{
+   private String sentence;
+   private char lookFor;
+
+	public LetterRemover()
+	{
+		//call set
+		sentence = " ";
+		lookFor = 0;
+	}
+
+	//add in second constructor
+	public LetterRemover(String s, char rem)
+	{
+		setRemover(s, rem);
+	}
+	
+	
+	public void setRemover(String s, char rem)
+	{
+		sentence = s;
+		lookFor = rem;
+	}
+
+	public String removeLetters()
+	{
+		String cleaned=sentence;
+		//int loc = cleaned.indexOf(lookFor);
+		
+		while (cleaned.indexOf(lookFor) != (-1))
+		{
+			cleaned = cleaned.substring(0, cleaned.indexOf(lookFor)) + cleaned.substring((cleaned.indexOf(lookFor))+1);
+		} 
+
+		return cleaned;
+	}
+
+	public String toString()
+	{
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters();
+	}
+}
