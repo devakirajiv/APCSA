0218a2897483b08b8cb80064085fbc594a65a6b6
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-22 07:50:34 -0800

    Finished Lab 11j

--- Unit 7/src/Triples.java
+++ Unit 7/src/Triples.java
@@ -13,36 +13,54 @@ public class Triples
 	public Triples()
 	{
 		this(0);
+		
 	}
 
 	public Triples(int num)
 	{
-
-
+		number = 0;
 	}
 
 	public void setNum(int num)
 	{
-
-
+		number = num;
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
+		for(int i = 1; i <= a; i++){
+			if (a % i ==0){
+				if(b % i ==0){
+					if(c % i ==0){
+						max = i;
+					}
+				}
+			}
+		}
 
 
 
-		return 1;
+		return max;
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
+		
+		for(int c=1; c < number; c++){
+			for(int b = 1; b < number; b++){
+				for(int a = 1; a < number-2; a++){
+					if(a*a+b*b == c*c && greatestCommonFactor(a,b,c) <= 1){
+						if((a%2 ==0 && b%2!=0) || (a%2 !=0 && b%2==0)){
+							if(c%2 !=0){
+								output = output + (a + " " + b + " " + c) + "\n";
+							}
+						}
+					}
+				}
+			}
+		}
 
 
 		return output+"\n";
