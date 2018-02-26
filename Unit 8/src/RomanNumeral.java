2dd92d0620a95aa808708e43d09d0ea2d350dfbb
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-26 07:43:23 -0800

    Finished Lab 14i

--- Unit 8/src/RomanNumeral.java
+++ Unit 8/src/RomanNumeral.java
@@ -20,40 +20,99 @@ public class RomanNumeral
 	public RomanNumeral(String str)
 	{
 
-
+		setRoman(str);
 
 	}
 
 	public RomanNumeral(Integer orig)
 	{
 
-
+		setNumber(orig);
 
 	}
 
 	public void setNumber(Integer num)
 	{
-
-
-
-
+		number = num;
 
 	}
 
 	public void setRoman(String rom)
 	{
-
-
-
+		roman = rom;
 	}
 
 	public Integer getNumber()
 	{
+		String modified = roman;
+		String firstTwo = modified.substring(0,2);
+		String first = modified.substring(0,1);
+		int total = 0;
+		
+		for (int i = 0; i<LETTERS.length; i++)
+		{
+			if (firstTwo.equals(LETTERS[i]))
+			{
+				modified = modified.substring(2);
+				total += NUMBERS[i];
+				i=-1;
+				if (modified.length()>=2)
+					firstTwo = modified.substring(0,2);
+				if (modified.length()>=1)
+					first = modified.substring(0,1);
+			}
+			else if (first.equals(LETTERS[i]))
+			{
+				modified = modified.substring(1);
+				total += NUMBERS[i];
+				i=-1;
+				if (modified.length()>=2)
+					firstTwo = modified.substring(0,2);
+				if (modified.length()>=1)
+					first = modified.substring(0,1);
+				
+			}
+			else if (modified.isEmpty()==true)
+			{
+				return total;
+			}
+		}
 		return number;
 	}
 
 	public String toString()
 	{
+		int modified = number;
+		String output ="";
+		for (int i = 0; i<NUMBERS.length; i++)
+		{
+			//checks to see if it is already in NUMBERS array
+			if (number == NUMBERS[i])
+			{
+				return LETTERS[i];
+			}
+			//checks to make sure number can be subtracted and be greater than
+			//zero, can't be zero on first try b/c already checked with first if
+			else if ((modified-NUMBERS[i]) > 0)
+			{
+				//new for loop inside 
+				for (int ii = 0; ii<NUMBERS.length; ii++)
+				{
+					//now it can be equal to zero
+					if ((modified-NUMBERS[ii]) >= 0)
+					{
+						modified = (modified-NUMBERS[ii]);
+						output+= LETTERS[ii];
+						
+						//set to -1 b/c it will increment to 0 when 
+						//for loop repeats
+						ii=-1;	
+					}
+					else if (modified ==0)
+						return output;
+				}
+			}
+		}
 		return roman + "\n";
 	}
 }
