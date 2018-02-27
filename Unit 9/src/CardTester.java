a7b6b718f08d8044499ac4f84ecda4d1b3493d85
devakirajiv <33033277+devakirajiv@users.noreply.github.com>
2018-02-27 09:14:59 -0800

    Finished first Card Activity

--- Unit 9/src/CardTester.java
+++ Unit 9/src/CardTester.java
@@ -8,6 +8,29 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
+		
+		Card test = new Card("King", "Hearts", 13);
+		Card test2 = new Card("Four", "Spades", 4);
+		Card test3 = new Card("Four", "Spades", 4);
+		
+		System.out.println(test);
+		test.matches(test2);
+		test.matches(test3);
+		
+		System.out.println(test2);
+		test.matches(test);
+		test.matches(test3);
+		
+		System.out.println(test3);
+		test.matches(test2);
+		test.matches(test);
+		
+		
+		
+		
+		
+		
+		
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
 	}
 }
